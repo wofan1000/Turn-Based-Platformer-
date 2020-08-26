d6ad938755ed154641602dcd64b025fd579c2aea
wofan1000 <70290060+wofan1000@users.noreply.github.com>
2020-08-26 15:50:37 -0700

    Create BattleMagic.cs

---
+++ PlayerController/BattleMagic.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BattleMagic : MonoBehaviour
+{
+
+    public string spellName;
+    public int spellCost;
+    public Text nameText;
+    public Text costText;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Press()
+    {
+        if (BattleManager.instance.activeBattlers[BattleManager.instance.currentTurn].currentMP >= spellCost)
+        {
+            BattleManager.instance.magicMenu.SetActive(false);
+            BattleManager.instance.OpenTargetMenu(spellName);
+            BattleManager.instance.activeBattlers[BattleManager.instance.currentTurn].currentMP -= spellCost;
+      } else
+        {
+            BattleManager.instance.BattleNotice.theText.text = " Not Enough MP!";
+            BattleManager.instance.BattleNotice.Activate();
+            BattleManager.instance.magicMenu.SetActive(false);
+        }
+    }
+}
